f4ba139918ba61c97b3b6977dbade63e21704803
soju6jan <cybersol@naver.com>
2019-04-22 16:07:00 +0900

    update

---
+++ start.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+COUNT=0
+
+while [ 1 ];
+do
+    git pull
+    FILENAME="update_requirements.txt"
+    if [ -f "$FILENAME" ] ; then
+        pip install -r update_requirements.txt
+    fi
+    python -OO sjva.py 9999 ${COUNT}
+    RESULT=$?
+    echo "EXIT CODE : ${RESULT}.............."
+    if [ "$RESULT" = "1" ] || [ "$RESULT" = "2" ]; then
+        echo 'REPEAT....'
+    else
+        echo 'FINISH....'
+        break
+    fi
+    COUNT=`expr $COUNT + 1`
+done
