66418f3d83a06f056e124c5a6bdd77e7e9b436c3
soju6jan <cybersol@naver.com>
2019-05-15 05:14:00 +0900

    a

--- static/file/sjva_tving.php
+++ static/file/sjva_tving.php
@@ -1,16 +1,4 @@
 <?php
-function log_str($str) {
-	$log_file = fopen("/volume1/web/tving/sjva_log.txt", "a");  
-	fwrite($log_file, $str."\r\n");  
-	fclose($log_file);  
-}
-
-function attack_log($str) {
-	$log_file = fopen("/volume1/web/attack.txt", "a");  
-	fwrite($log_file, $str."\r\n");  
-	fclose($log_file);  
-}
-
 function get_html() {
 	$url = func_get_arg(0);
 	$headers = func_get_arg(1);
@@ -22,8 +10,6 @@ function get_html() {
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
-	//curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookies.txt');
-	//curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookies.txt');
 	if ( $query != null) {
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
@@ -35,14 +21,8 @@ function get_html() {
 $url = $_POST['url'];
 $cookie = $_POST['cookie'];
 
-log_str($url);
-log_str($cookie);
-
 $headers = array('Cookie: '.$cookie.';');
 $data = get_html($url, $headers);
 header('Content-type: application/json');
 echo $data;
-
-#echo json_encode($array);
-	
 ?> 
